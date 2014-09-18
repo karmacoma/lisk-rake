da8c450fce6a4fb790b36fae7a020b6d03b2e749
Oliver Beddows <oliver.beddows@googlemail.com>
2014-09-18 21:16:07 +0100

    Encoding url(s).

--- lib/crypti_api.rb
+++ lib/crypti_api.rb
@@ -1,11 +1,13 @@
+require 'uri'
+
 class CryptiApi
   def initialize(task)
     @task = task
   end
 
-  def get(url, &block)
+  def get(url, data = nil, &block)
     begin
-      body = @task.capture 'curl', '-X', 'GET', '-H', '"Content-Type: application/json"', 'http://127.0.0.1:6040' + url
+      body = @task.capture 'curl', '-X', 'GET', '-H', '"Content-Type: application/json"', encode_url(url, data)
       json = JSON.parse(body)
     rescue Exception => exception
       @task.info error_message
@@ -17,7 +19,7 @@ class CryptiApi
 
   def post(url, data, &block)
     begin
-      body = @task.capture 'curl', '-X', 'POST', '-H', '"Content-Type: application/json"', '-d', "'#{data.to_json}'", 'http://127.0.0.1:6040' + url
+      body = @task.capture 'curl', '-X', 'POST', '-H', '"Content-Type: application/json"', '-d', "'#{data.to_json}'", encode_url(url)
       json = JSON.parse(body)
     rescue Exception => exception
       @task.info error_message
@@ -25,6 +27,12 @@ class CryptiApi
     end
     block.call json if block_given?
     return json
+  end
+
+  def encode_url(url, data = nil)
+    _url = 'http://127.0.0.1:6040' + url
+    _url + '?' + URI.encode_www_form(data) if data
+    _url
   end
 
   def error_message
