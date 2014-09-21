a74e04abcf6f0b8b4667afbddf2d6bd22d2f3960
Oliver Beddows <oliver.beddows@googlemail.com>
2014-09-21 21:39:59 +0100

    Moving new line character(s).

--- lib/forging_status.rb
+++ lib/forging_status.rb
@@ -4,10 +4,10 @@ class ForgingStatus
   end
 
   def forging_enabled
-    [sprintf("%-13s", 'Forging:'), @json['forgingEnabled']]
+    [sprintf("%-13s", 'Forging:'), @json['forgingEnabled'], "\n"]
   end
 
   def to_s
-    [forging_enabled, "\n"].join.to_s
+    [forging_enabled].join.to_s
   end
 end
