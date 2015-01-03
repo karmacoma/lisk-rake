1be7d12428fbd626f76cf7f88b2582fccb43db38
Oliver Beddows <oliver.beddows@googlemail.com>
2015-01-03 09:26:22 +0000

    Merging LoadingStatus#height with blocks_count.
    Skipping when blockchain has loaded.

--- lib/loading_status.rb
+++ lib/loading_status.rb
@@ -9,15 +9,13 @@ module CryptiKit
     end
 
     def height
-      [sprintf("%-19s", 'Height:'), @json['height'].to_i, "\n"]
-    end
-
-    def blocks_count
-      [sprintf("%-19s", 'Blocks:'), @json['blocksCount'].to_i, "\n"]
+      unless @json['loaded'] then
+        [sprintf("%-19s", 'Height:'), @json['height'].to_i, ' / ', @json['blocksCount'].to_i, "\n"]
+      end
     end
 
     def to_s
-      [loaded, height, blocks_count].join.to_s
+      [loaded, height].join.to_s
     end
   end
 end
