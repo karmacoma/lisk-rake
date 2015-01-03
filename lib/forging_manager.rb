818491df6c6880addc5630c28bf3b1fcf7043aef
Oliver Beddows <oliver.beddows@googlemail.com>
2015-01-03 20:23:07 +0000

    Initializing Curl class only once.

--- lib/forging_manager.rb
+++ lib/forging_manager.rb
@@ -2,13 +2,13 @@ module CryptiKit
   class ForgingManager
     def initialize(task)
       @task = task
+      @api  = CryptiKit::Curl.new(@task)
     end
 
     def start(server, node)
       node.get_passphrase do |passphrase|
-        api = CryptiKit::Curl.new(@task)
-        api.post '/forgingApi/startForging', passphrase
-        api.post '/api/unlock', passphrase do |json|
+        @api.post '/forgingApi/startForging', passphrase
+        @api.post '/api/unlock', passphrase do |json|
           manager = CryptiKit::AccountManager.new(@task, server)
           manager.add(json, passphrase)
         end
@@ -17,8 +17,7 @@ module CryptiKit
 
     def stop(server, node)
       node.get_passphrase do |passphrase|
-        api = CryptiKit::Curl.new(@task)
-        api.post '/forgingApi/stopForging', passphrase do |json|
+        @api.post '/forgingApi/stopForging', passphrase do |json|
           manager = CryptiKit::AccountManager.new(@task, server)
           manager.remove(json)
         end
