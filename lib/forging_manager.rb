7d9c25d43cd5e9805cdc17d01dc646142496a0ae
Oliver Beddows <oliver.beddows@googlemail.com>
2015-01-03 20:24:30 +0000

    Removing namespaces.

--- lib/forging_manager.rb
+++ lib/forging_manager.rb
@@ -2,14 +2,14 @@ module CryptiKit
   class ForgingManager
     def initialize(task)
       @task = task
-      @api  = CryptiKit::Curl.new(@task)
+      @api  = Curl.new(@task)
     end
 
     def start(server, node)
       node.get_passphrase do |passphrase|
         @api.post '/forgingApi/startForging', passphrase
         @api.post '/api/unlock', passphrase do |json|
-          manager = CryptiKit::AccountManager.new(@task, server)
+          manager = AccountManager.new(@task, server)
           manager.add(json, passphrase)
         end
       end
@@ -18,7 +18,7 @@ module CryptiKit
     def stop(server, node)
       node.get_passphrase do |passphrase|
         @api.post '/forgingApi/stopForging', passphrase do |json|
-          manager = CryptiKit::AccountManager.new(@task, server)
+          manager = AccountManager.new(@task, server)
           manager.remove(json)
         end
       end
