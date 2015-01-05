6129921a62dae6cce538778d85c35b1ba32b0515
Oliver Beddows <oliver.beddows@googlemail.com>
2015-01-05 16:39:12 +0000

    Handling fingerprint mismatch error(s).

--- lib/server_error.rb
+++ lib/server_error.rb
@@ -4,6 +4,7 @@ module CryptiKit
   class ServerError < BasicError
     @errors = {
       /Authentication failed/i  => '=> Authentication failed.',
+      /Fingerprint [a-z0-9:]+/i => '=> Fingerprint mismatch.',
       /Network is unreachable/i => '=> Network is unreachable.',
       /Connection closed/i      => '=> Connection closed by remote host.',
       /Connection timed out/i   => '=> Connection timed out.',
