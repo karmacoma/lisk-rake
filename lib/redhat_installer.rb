87d9026b0091399f56eb2b9dd8fd2124f3c25175
Oliver Beddows <oliver.beddows@googlemail.com>
2015-01-08 15:14:13 +0000

    Adding sqlite3 dependency.

--- lib/redhat_installer.rb
+++ lib/redhat_installer.rb
@@ -8,7 +8,7 @@ module CryptiKit
       @task.info 'Updating package lists...'
       @task.execute 'yum', 'clean', 'expire-cache'
       @task.info 'Installing packages...'
-      @task.execute 'yum', 'install', '-y', 'gcc-c++', 'make', 'curl', 'python', 'wget', 'unzip'
+      @task.execute 'yum', 'install', '-y', 'gcc-c++', 'make', 'curl', 'python', 'sqlite', 'wget', 'unzip'
       @task.info 'Adding nodejs repository...'
       @task.execute 'curl', '-sL', 'https://rpm.nodesource.com/setup', '|', 'bash', '-'
       @task.info 'Installing nodejs...'
