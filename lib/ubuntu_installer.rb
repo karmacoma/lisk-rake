f3911139f105dea872e27f1e7c2d8a67065fce98
Oliver Beddows <oliver.beddows@googlemail.com>
2015-12-04 20:38:00 +0000

    Requiring and using sudo to install packages.

--- lib/ubuntu_installer.rb
+++ lib/ubuntu_installer.rb
@@ -5,12 +5,12 @@ module CryptiKit
     end
 
     def install(node, deps)
-      deps.check_remote(node, 'apt-get')
+      deps.check_remote(node, 'sudo', 'apt-get')
 
       @task.info 'Updating package lists...'
-      @task.execute 'apt-get', 'update'
+      @task.execute 'sudo', 'apt-get', 'update'
       @task.info 'Installing packages...'
-      @task.execute 'apt-get', 'install', '-f', '--yes', 'bash', 'curl', 'wget', 'unzip'
+      @task.execute 'sudo', 'apt-get', 'install', '-f', '--yes', 'bash', 'curl', 'wget', 'unzip'
       @task.info '=> Done.'
     end
   end
