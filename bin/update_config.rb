598288eb5f6085f53d6b30da1d045832f2664388
Olivier Beddows <oliver.beddows@googlemail.com>
2015-12-10 16:44:41 +0000

    Reworking bin/update_config.rb.
    - Handling missing or empty files.
    - Migrating servers from previous config.
    - Refactoring code into singleton class.

--- bin/update_config.rb
+++ bin/update_config.rb
@@ -1,25 +1,63 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
+require 'singleton'
 
-file_a = File.join(Dir.pwd, 'config.bak')
+class ConfigUpdater
+  include Singleton
 
-if File.exist?(file_a) then
-  config_bak = YAML.load_file(file_a)
-end
+  @config_bak = @config_yml = {}
 
-file_b = File.join(Dir.pwd, 'config.yml')
+  def self.load_config(extension)
+    file = File.join(Dir.pwd, "config.#{extension}")
 
-if File.exist?(file_b) then
-  config_yml = YAML.load_file(file_b)
-end
+    if File.exist?(file) then
+      yaml = YAML.load_file(file)
 
-if config_bak.is_a?(Hash) and config_yml.is_a?(Hash) then
-  ['servers', 'accounts'].each do |k|
-    config_yml[k] = config_bak[k]
+      if yaml.is_a?(Hash) then
+        return yaml
+      else
+        raise "Invalid config.#{extension} data."
+      end
+    else
+      raise "Missing config.#{extension} file."
+    end
   end
 
-  File.open('config.yml', 'w') do |f|
-    f.write config_yml.to_yaml
+  def self.migrate_server(s)
+    {
+      'hostname' => s[1].to_s,
+      'accounts' => @config_bak['accounts'][s[0].to_i].to_a
+    }
+  end
+
+  def self.migrate_servers
+    @config_yml['servers'] = @config_bak['servers'].collect do |s|
+      [s[0].to_i, self.migrate_server(s)]
+    end.to_h
+    @config_yml.delete('accounts')
+  end
+
+  def self.update_servers
+    return unless @config_bak['servers'].is_a?(Hash)
+
+    if @config_bak['accounts'].is_a?(Hash) then
+      migrate_servers
+    else
+      @config_yml['servers'] = @config_bak['servers']
+    end
+  end
+
+  def self.run
+    @config_bak = self.load_config('bak')
+    @config_yml = self.load_config('yml')
+
+    update_servers
+
+    File.open('config.yml', 'w') do |f|
+      f.write @config_yml.to_yaml
+    end
   end
 end
+
+ConfigUpdater.run
