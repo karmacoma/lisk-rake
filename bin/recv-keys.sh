dc79b2ca535703d342a35119e8839efcdd268da4
Olivier Beddows <olivier@lisk.io>
2016-04-06 15:41:58 +0100

    Updating gpg keys.

--- bin/recv-keys.sh
+++ bin/recv-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if command -v gpg >/dev/null 2>&1 ; then
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 fi
